986d55211cca98128bb0f14d201f4836b89d3461
SAUD ALNASSER <saud4services@gmail.com>
2024-04-19 02:26:24 +0300

    feat: create basic return statements parser

--- src/parser/quantifiers.rs
+++ src/parser/quantifiers.rs
@@ -72,3 +72,45 @@ impl Quantifier for LetStatementQuantifier {
         })
     }
 }
+
+pub struct ReturnStatementQuantifier;
+
+impl Quantifier for ReturnStatementQuantifier {
+    fn is_applicable(&self, lexer: &mut Peekable<Lexer>) -> bool {
+        match lexer.peek() {
+            Some(Token {
+                kind: TokenKind::RETURN,
+                ..
+            }) => true,
+            _ => false,
+        }
+    }
+
+    fn process(&self, lexer: &mut Peekable<Lexer>) -> Result<Statement, Box<dyn Error>> {
+        let token = match lexer.next() {
+            Some(token) => token,
+            None => return Err("Expected keyword return".into()),
+        };
+
+        // TODO: manage all kinds of expressions
+
+        let expression = match lexer.next() {
+            Some(Token {
+                kind: TokenKind::INT,
+                literal,
+                ..
+            }) => Expression::INT { value: literal },
+            _ => return Err("Expected expression".into()),
+        };
+
+        match lexer.next() {
+            Some(Token {
+                kind: TokenKind::SEMICOLON,
+                ..
+            }) => (),
+            _ => return Err("Expected semicolon".into()),
+        }
+
+        Ok(Statement::Return { token, expression })
+    }
+}
