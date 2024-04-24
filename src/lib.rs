5f9af99a49fd96a8658543c667cd287657dae7a3
SAUD ALNASSER <saud4services@gmail.com>
2024-04-24 18:04:30 +0300

    feat: create a baseline for the evaluator

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod ast;
+mod eval;
 mod lexer;
 mod parser;
 mod repl;
 mod utils;
 
 pub use ast::*;
+pub use eval::Evaluator;
 pub use lexer::Lexer;
 pub use parser::Parser;
 pub use repl::REPL;
