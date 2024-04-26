41851764a286b3e36b8ce82956407f284f067679
saud alnasser <123410712+saud-alnasser@users.noreply.github.com>
2024-04-26 07:41:46 +0300

    feat: eval infix expressions (#48)
    ## 🎯 Changes
    
    extended the evaluator to eval infix expressions.

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -47,6 +47,34 @@ impl Evaluator {
 
                 DataType::NULL
             }
+            Expression::INFIX(infix) => {
+                let left = Evaluator::eval_expression(*infix.left);
+                let right = Evaluator::eval_expression(*infix.right);
+
+                if let (DataType::INT(left), DataType::INT(right)) = (&left, &right) {
+                    match infix.operator.kind {
+                        TokenKind::PLUS => return DataType::INT(left + right),
+                        TokenKind::MINUS => return DataType::INT(left - right),
+                        TokenKind::ASTERISK => return DataType::INT(left * right),
+                        TokenKind::SLASH => return DataType::INT(left / right),
+                        TokenKind::LT => return DataType::BOOLEAN(left < right),
+                        TokenKind::GT => return DataType::BOOLEAN(left > right),
+                        TokenKind::EQ => return DataType::BOOLEAN(left == right),
+                        TokenKind::NEQ => return DataType::BOOLEAN(left != right),
+                        _ => (),
+                    }
+                }
+
+                if let (DataType::BOOLEAN(left), DataType::BOOLEAN(right)) = (&left, &right) {
+                    match infix.operator.kind {
+                        TokenKind::EQ => return DataType::BOOLEAN(left == right),
+                        TokenKind::NEQ => return DataType::BOOLEAN(left != right),
+                        _ => (),
+                    }
+                }
+
+                DataType::NULL
+            }
             _ => DataType::NULL,
         }
     }
@@ -66,8 +94,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_eval_integer_expression() {
-        let tests = vec![("5;", 5), ("10;", 10), ("-5;", -5), ("-10;", -10)];
+    fn test_eval_integer_expressions() {
+        let tests = vec![
+            ("5;", 5),
+            ("10;", 10),
+            ("-5;", -5),
+            ("-10;", -10),
+            ("5 + 5 + 5 + 5 - 10;", 10),
+            ("2 * 2 * 2 * 2 * 2;", 32),
+            ("-50 + 100 + -50;", 0),
+            ("5 * 2 + 10;", 20),
+            ("5 + 2 * 10;", 25),
+            ("20 + 2 * -10;", 0),
+            ("50 / 2 * 2 + 10;", 60),
+            ("2 * (5 + 10);", 30),
+            ("3 * 3 * 3 + 10;", 37),
+            ("3 * (3 * 3) + 10;", 37),
+            ("(5 + 10 * 2 + 15 / 3) * 2 + -10;", 50),
+        ];
 
         for (input, expected) in tests {
             let lexer = Lexer::new(input);
@@ -85,25 +129,9 @@ mod tests {
 
     #[test]
     fn test_eval_boolean_expression() {
-        let tests = vec![("true;", true), ("false;", false)];
-
-        for (input, expected) in tests {
-            let lexer = Lexer::new(input);
-            let mut parser = Parser::new(lexer);
-            let program = parser.parse().unwrap();
-
-            for statement in program.statements {
-                match Evaluator::eval(statement) {
-                    DataType::BOOLEAN(value) => assert_eq!(value, expected),
-                    _ => panic!("expected a boolean, got something else"),
-                }
-            }
-        }
-    }
-
-    #[test]
-    fn test_bang_operator() {
         let tests = vec![
+            ("true;", true),
+            ("false;", false),
             ("!true;", false),
             ("!false;", true),
             ("!!true;", true),
@@ -112,6 +140,23 @@ mod tests {
             ("!!5;", true),
             ("!-5;", false),
             ("!!-5;", true),
+            ("1 < 2;", true),
+            ("1 > 2;", false),
+            ("1 < 1;", false),
+            ("1 > 1;", false),
+            ("1 == 1;", true),
+            ("1 != 1;", false),
+            ("1 == 2;", false),
+            ("1 != 2;", true),
+            ("true == true;", true),
+            ("false == false;", true),
+            ("true == false;", false),
+            ("true != false;", true),
+            ("false != true;", true),
+            ("(1 < 2) == true;", true),
+            ("(1 < 2) == false;", false),
+            ("(1 > 2) == true;", false),
+            ("(1 > 2) == false;", true),
         ];
 
         for (input, expected) in tests {
