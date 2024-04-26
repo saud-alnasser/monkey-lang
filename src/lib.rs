339cac158195b3f21bf87d8f307febbf180b2e65
saud alnasser <123410712+saud-alnasser@users.noreply.github.com>
2024-04-26 07:40:27 +0300

    feat: eval prefix expressions (#47)
    ## 🎯 Changes
    
    extended the evaluator to eval prefix expressions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod ast;
-mod eval;
+mod evaluator;
 mod lexer;
 mod parser;
 mod repl;
 mod utils;
 
 pub use ast::*;
-pub use eval::Evaluator;
+pub use evaluator::Evaluator;
 pub use lexer::Lexer;
 pub use parser::Parser;
 pub use repl::REPL;
