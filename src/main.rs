e14d653ff12002071d42e5eb936bcac535745ec2
SAUD ALNASSER <saud4services@gmail.com>
2024-04-30 07:22:43 +0300

    feat: increase stack limit from 2mb to 16mb

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use monkey_lang::REPL;
+use std::thread;
 
 fn main() {
-    REPL::run();
+    let stack_size = 16 * 1024 * 1024; // 16MB
+    let builder = thread::Builder::new().stack_size(stack_size);
+
+    let handle = builder
+        .spawn(|| {
+            REPL::run();
+        })
+        .expect("failed to create a thread for the REPL");
+
+    handle.join().expect("REPL thread has panicked");
 }
