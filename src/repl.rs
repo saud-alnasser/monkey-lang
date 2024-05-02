a7110bad61f6d8c626bae8aa938faf357e2c359e
SAUD ALNASSER <saud4services@gmail.com>
2024-05-02 03:30:39 +0300

    refactor: change evaluator to execute a program

--- src/repl.rs
+++ src/repl.rs
@@ -24,22 +24,12 @@ static MONKEY_FACE: &str = r#"
 pub struct REPL;
 
 impl REPL {
-    fn eval(code: &str, env: Rc<RefCell<Environment>>) -> Result<DataType, Box<dyn Error>> {
+    fn execute(code: &str, env: Rc<RefCell<Environment>>) -> Result<DataType, Box<dyn Error>> {
         let lexer = Lexer::new(code);
         let mut parser = Parser::new(lexer);
         let program = parser.parse()?;
 
-        let mut output = DataType::NULL;
-
-        for statement in program.statements {
-            output = Evaluator::eval(statement, Rc::clone(&env))?;
-
-            if let DataType::RETURN(_) = output {
-                break;
-            }
-        }
-
-        Ok(output)
+        Evaluator::execute(program, env)
     }
 
     pub fn run() {
@@ -55,7 +45,7 @@ impl REPL {
             io::stdout().flush().unwrap();
             io::stdin().read_line(&mut code).unwrap();
 
-            match REPL::eval(&code, Rc::clone(&env)) {
+            match REPL::execute(&code, Rc::clone(&env)) {
                 Ok(data) => match data {
                     DataType::NULL => (),
                     _ => println!("{}", data),
