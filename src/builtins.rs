99ce586d50c1d541469e832ede2ae07c06486246
SAUD ALNASSER <saud4services@gmail.com>
2024-05-03 01:20:49 +0300

    feat: implement builtin first function

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,19 +1,20 @@
 use crate::DataType;
 
-pub fn builtins() -> [(&'static str, DataType); 1] {
-    [(
-        "len",
-        DataType::BUILTIN {
-            func: |args| {
-                if args.len() != 1 {
-                    return Err(format!(
-                        r#"extra arguments are passed to BUILTIN("len"). got={}, want=1"#,
-                        args.len()
-                    )
-                    .into());
-                }
+pub fn builtins() -> [(&'static str, DataType); 2] {
+    [
+        (
+            "len",
+            DataType::BUILTIN {
+                func: |args| {
+                    if args.len() != 1 {
+                        return Err(format!(
+                            r#"extra arguments are passed to BUILTIN("len"). got={}, want=1"#,
+                            args.len()
+                        )
+                        .into());
+                    }
 
-                match &args[0] {
+                    match &args[0] {
                         DataType::STRING(s) => Ok(DataType::INT(s.len() as i64)),
                         DataType::ARRAY(a) => Ok(DataType::INT(a.len() as i64)),
                         _ => Err(format!(
@@ -22,7 +23,37 @@ pub fn builtins() -> [(&'static str, DataType); 1] {
                         )
                         .into()),
                     }
+                },
             },
-        },
-    )]
+        ),
+        (
+            "first",
+            DataType::BUILTIN {
+                func: |args| {
+                    if args.len() != 1 {
+                        return Err(format!(
+                            r#"extra arguments are passed to BUILTIN("first"). got={}, want=1"#,
+                            args.len()
+                        )
+                        .into());
+                    }
+
+                    match &args[0] {
+                        DataType::ARRAY(array) => {
+                            if array.is_empty() {
+                                return Ok(DataType::NULL);
+                            }
+
+                            Ok(array[0].clone())
+                        },
+                        _ => Err(format!(
+                            r#"argument passed to BUILTIN("first") is not supported. got={:?}, want=ARRAY"#,
+                            args[0]
+                        )
+                        .into()),
+                    }
+                },
+            },
+        ),
+    ]
 }
