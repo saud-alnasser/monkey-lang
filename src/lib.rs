1930397fee0afcb2a0df432ea0d29ee690345ab6
SAUD ALNASSER <saud4services@gmail.com>
2024-09-01 23:41:51 +0300

    refactor: split lexer into multiple files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod ast;
 mod builtins;
 mod environment;
+mod error;
 mod evaluator;
 mod lexer;
 mod parser;
@@ -9,6 +10,7 @@ mod repl;
 pub use ast::*;
 pub use builtins::*;
 pub use environment::*;
+pub use error::*;
 pub use evaluator::*;
 pub use lexer::*;
 pub use parser::*;
