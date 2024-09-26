6ae26b95654349c145018d3466d7f183dbb4d311
saud alnasser <123410712+saud-alnasser@users.noreply.github.com>
2024-09-26 05:57:01 +0300

    feat: implement hash literals (#99)

--- src/evaluator/datatype.rs
+++ src/evaluator/datatype.rs
@@ -1,7 +1,25 @@
+use std::collections::HashMap;
 use std::{cell::RefCell, fmt::Display, rc::Rc};
 
 use crate::builtins::error::Result;
 use crate::{BlockExpression, Environment, IdentExpression};
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum HashKey {
+    String(Box<str>),
+    Int(i64),
+    Boolean(bool),
+}
+
+impl Display for HashKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HashKey::String(value) => write!(f, "{}", value),
+            HashKey::Int(value) => write!(f, "{}", value),
+            HashKey::Boolean(value) => write!(f, "{}", value),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum DataType {
@@ -15,6 +33,9 @@ pub enum DataType {
         parameters: Vec<IdentExpression>,
         body: BlockExpression,
         env: Rc<RefCell<Environment>>,
+    },
+    HASH {
+        pairs: HashMap<HashKey, DataType>,
     },
     BUILTIN {
         func: fn(Vec<DataType>) -> Result<DataType>,
@@ -41,6 +62,23 @@ impl Display for DataType {
             DataType::RETURN(value) => write!(f, "{}", value),
             DataType::IDENT(value) => write!(f, "{}", value),
             DataType::FUNCTION { .. } => write!(f, ""),
+            DataType::HASH { pairs: map } => {
+                let mut output = String::new();
+
+                let mut pairs = String::new();
+
+                for (i, (key, value)) in map.iter().enumerate() {
+                    pairs.push_str(&format!(" {}: {}", key, value));
+
+                    if i < pairs.len() - 1 {
+                        pairs.push_str(", ");
+                    }
+                }
+
+                output.push_str(&format!("{{{}}}", pairs));
+
+                write!(f, "{}", output)
+            }
             DataType::BUILTIN { .. } => write!(f, ""),
             DataType::UNDEFINED => write!(f, ""),
             DataType::NULL => write!(f, "null"),
